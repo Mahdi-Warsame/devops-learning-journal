43063bef4ab0122e8d6f94286ae139556cb4ca54
Mahdi-Warsame <m.warsame25@yahoo.com>
2026-07-09 16:23:42 +0100

    latest commit: docker module end

--- docker/hello_flask/app.py
+++ docker/hello_flask/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 import MySQLdb
 
-import MySQLdb
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,4 +18,4 @@ def hello_world():
     return f'Yeeeey Hello, World! MySQL version: {version[0]}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(host='0.0.0.0', port=5002)
